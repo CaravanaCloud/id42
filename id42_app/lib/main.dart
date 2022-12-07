cbde0a0c71b4cc90c313cdfda7c819178196e63e
Julio Faerman <jufaerma@redhat.com>
2022-12-07 12:05:03 +0100

    wip

--- id42_app/lib/main.dart
+++ id42_app/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'dart:html';
-import 'package:flutter/material.dart';
-import 'package:google_maps/google_maps.dart' hide Icon;
 import 'dart:ui' as ui;
+import 'package:latlong2/latlong.dart';
+import 'package:flutter_map/flutter_map.dart';
+
+
 
 void main() {
   runApp(const MyApp());
@@ -66,36 +67,24 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget getMap() {
-    String htmlId = "7";
-
-    // ignore: undefined_prefixed_name
-    ui.platformViewRegistry.registerViewFactory(htmlId, (int viewId) {
-      final myLatlng = LatLng(1.3521, 103.8198);
-
-      final mapOptions = MapOptions()
-        ..zoomControlOptions = (ZoomControlOptions()..position = ControlPosition.LEFT_BOTTOM)
-        ..streetViewControlOptions = (StreetViewControlOptions()..position = ControlPosition.LEFT_BOTTOM)
-        ..zoom = 11
-        ..center = LatLng(41.385063, 2.173404);
-
-      final elem = DivElement()
-        ..id = htmlId
-        ..style.width = "100%"
-        ..style.height = "100%"
-        ..style.border = 'none';
-
-      final map = GMap(elem, mapOptions);
-
-      Marker(MarkerOptions()
-        ..position = myLatlng
-        ..map = map
-        ..title = 'Hello World!'
-      );
-
-      return elem;
-    });
-
-    return HtmlElementView(viewType: htmlId);
+    return FlutterMap(
+      options: MapOptions(
+        center: LatLng(41.3874, 2.1686),
+        zoom: 9.2,
+      ),
+      nonRotatedChildren: [
+        AttributionWidget.defaultWidget(
+          source: 'OpenStreetMap contributors',
+          onSourceTapped: null,
+        ),
+      ],
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+      ],
+    );
   }
 
   void noop(){}
@@ -112,57 +101,5 @@ class _MyHomePageState extends State<MyHomePage> {
         )
         )
       );
-  }
-
-
-  Widget build_old(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
   }
 }
