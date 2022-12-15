6381a9a74fe009906225adaee496c2cc33debd77
Julio Faerman <jufaerma@redhat.com>
2022-12-15 23:39:55 +0100

    Live!

--- id42_cdk/src/main/java/com/myorg/Id42CdkStack.java
+++ id42_cdk/src/main/java/com/myorg/Id42CdkStack.java
@@ -1,10 +1,15 @@
 package com.myorg;
 
+import software.amazon.awscdk.CfnOutput;
 import software.amazon.awscdk.services.apigateway.Deployment;
 import software.amazon.awscdk.services.certificatemanager.Certificate;
 import software.amazon.awscdk.services.cloudfront.BehaviorOptions;
 import software.amazon.awscdk.services.cloudfront.Distribution;
 import software.amazon.awscdk.services.cloudfront.origins.S3Origin;
+import software.amazon.awscdk.services.ec2.DefaultInstanceTenancy;
+import software.amazon.awscdk.services.ec2.SubnetConfiguration;
+import software.amazon.awscdk.services.ec2.SubnetType;
+import software.amazon.awscdk.services.ec2.Vpc;
 import software.amazon.awscdk.services.iam.AnyPrincipal;
 import software.amazon.awscdk.services.iam.PolicyStatement;
 import software.amazon.awscdk.services.s3.Bucket;
@@ -28,6 +33,28 @@ public class Id42CdkStack extends Stack {
         super(scope, id, props);
         //TODO: Load from context https://docs.aws.amazon.com/cdk/v2/guide/context.html
         var domainNames = List.of("*.id42.cc","id42.cc");
+
+        // Rede
+        var subnets = SubnetConfiguration.builder()
+                .cidrMask(24)
+                .name("id42-subnet")
+                .subnetType(SubnetType.PRIVATE_ISOLATED)
+                .build();
+
+        var vpc = Vpc.Builder.create(this, "id42-vpc")
+                .maxAzs(3)
+                .subnetConfiguration(List.of(subnets))
+                .cidr("10.0.0.0/16")
+                .defaultInstanceTenancy(DefaultInstanceTenancy.DEFAULT)
+                .enableDnsHostnames(true)
+                .enableDnsSupport(true)
+                .build();
+
+        var outVpcId = CfnOutput.Builder.create(this, "pbnk-out-vpcId")
+                .value(vpc.getVpcId())
+                .build();
+
+        // Web
         var certificate = Certificate.fromCertificateArn(this, "WebAppCertificate", DEFAULT_CERTIFICANTE_ARN);
 
         var bucket = Bucket.Builder.create(this, "WebAppBucket")
