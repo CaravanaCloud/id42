ea0257b330c58fcfb95810e81d83b52364a28555
Julio Faerman <jufaerma@redhat.com>
2022-12-30 09:32:33 +0100

    1.9.6 initial request delivery intent (spanish)

---
+++ deploy-environment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+
+#BOT_BUCKET="$(aws cloudformation describe-stacks --stack-name "Id42BotAppStack" --query 'Stacks[0].Outputs[?OutputKey==`BotBucketName`].OutputValue' --output text)"
+#echo $BOT_BUCKET
+#aws s3 cp id42_bot/target/dist/* s3://$BOT_BUCKET/
+pushd id42_cdk
+cdk deploy --require-approval=never Id42BotEnvironmentStack
+popd
+
+echo "id42 deploy completed"
