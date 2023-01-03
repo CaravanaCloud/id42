2a9f2a83a61fb41fe272e2200cf89af1582b0b2e
Julio Faerman <jufaerma@redhat.com>
2023-01-03 12:43:03 +0100

    Improved deployment scripts and clock emojis

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ex
 
-BOT_BUCKET="id42botappstack-botbucketab77be32-1uq2ndywa2a8v"
+BOT_BUCKET="$(aws cloudformation describe-stacks --stack-name "Id42BotAppStack" --query 'Stacks[0].Outputs[?OutputKey==`BotBucketName`].OutputValue' --output text)"
+echo $BOT_BUCKET
 aws s3 cp id42_bot/target/dist/* s3://$BOT_BUCKET/
 pushd id42_cdk
 cdk deploy --require-approval=never --all
