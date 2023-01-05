cf0e46acf63d825f8d9a6ee5cdf9e23107c37f06
Julio Faerman <jufaerma@redhat.com>
2023-01-05 13:46:57 +0100

    WIP

--- install.sh
+++ install.sh
@@ -2,13 +2,15 @@
 set -ex
 
 echo "build java modules"
-mvn install
+mvn -f id42_core install
+mvn -f id42_api install
+mvn -f id42_bot install
 mkdir -p id42_bot/target/dist
+echo "copy bot runner to dist"
+cp id42_bot/target/*-runner.jar "id42_bot/target/dist"
 echo "build flutter module"
 pushd id42_app && ./build.sh && popd
-echo "build java modules"
+echo "build cdk module"
 mvn -f id42_cdk install
-echo "copy bot runner to s3"
-cp id42_bot/target/*-runner.jar "id42_bot/target/dist"
 
 echo "build done."
