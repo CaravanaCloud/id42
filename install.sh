0ebff4f60456273c233ab8afaf106e3fb2d3441d
Julio Faerman <jufaerma@redhat.com>
2023-01-12 20:11:40 +0100

    Improved CI

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 set -ex
 
 echo "build java modules"
-export MAVEN_ARGS="-DskipTests install"
+MAVEN_ARGS="-DskipTests -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn"
+MAVEN_ARGS="$MAVEN_ARGS install"
 mvn -f id42_kernel $MAVEN_ARGS
 mvn -f id42_core $MAVEN_ARGS
 mvn -f id42_api $MAVEN_ARGS
