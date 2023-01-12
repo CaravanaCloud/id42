d170654a22a029cfffb78d825b00c50b58f9751c
Julio Faerman <jufaerma@redhat.com>
2023-01-12 21:10:42 +0100

    Improved CI - Jacoco

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -ex
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "build java modules"
-MAVEN_ARGS="-B -DskipTests -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn"
+MAVEN_ARGS="-B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn"
 MAVEN_ARGS="$MAVEN_ARGS install"
 mvn -f id42_kernel $MAVEN_ARGS
 mvn -f id42_core $MAVEN_ARGS
 mvn -f id42_api $MAVEN_ARGS
 mvn -f id42_bot $MAVEN_ARGS
-mkdir -p id42_bot/target/dist
+mkdir -p "$DIR/id42_bot/target/dist"
 echo "copy bot runner to dist"
-cp id42_bot/target/*-runner.jar "id42_bot/target/dist"
+cp id42_bot/target/*-runner.jar "$DIR/id42_bot/target/dist"
 echo "build flutter module"
 pushd id42_app && ./build.sh && popd
 echo "build cdk module"
